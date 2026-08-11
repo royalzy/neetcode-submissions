b630591f911d907ee3cefcf4bf96f6a588a40e68
Royal <64013399+royalzy@users.noreply.github.com>
2026-08-11 14:56:09 +0800

    Add: is-palindrome - submission-0

---
+++ Data Structures & Algorithms/is-palindrome/submission-0.py
@@ -0,0 +1,15 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        clean_text = "".join(c.lower() for c in s if c.isalnum() and ord(c) < 128)
+
+        start = ""
+        end = ""
+
+        for i in range(len(clean_text)//2):
+            start = clean_text[i]
+            end = clean_text[len(clean_text) - i -1]
+
+            if start != end:
+                return False
+
+        return True
